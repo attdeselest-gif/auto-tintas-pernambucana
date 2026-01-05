90f3e92baa558f030ddae670aaa37f9213b367ca
attdeselest-gif <attdeselest@gmail.com>
2026-01-04 22:39:49 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Auto Tintas Pernambucana</h1>
-      <p>Sistema carregado com sucesso.</p>
-    </main>
-  );
-}
