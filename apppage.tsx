8a60b0af1501e6b1ba6a79486dacf2700c1575b6
attdeselest-gif <attdeselest@gmail.com>
2026-01-04 22:44:04 -0300

    Add files via upload

---
+++ apppage.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Auto Tintas Pernambucana</h1>
+      <p>Sistema online de controle de mercadorias</p>
+    </main>
+  );
+}
